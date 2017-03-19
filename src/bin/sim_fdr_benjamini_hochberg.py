090685e6804e2006303e76211943b3f9328e3d3a
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2017-03-19 16:12:19 -0400

    Labeling pvals as randomly generated for any value or generated to be significant.

--- src/bin/sim_fdr_benjamini_hochberg.py
+++ src/bin/sim_fdr_benjamini_hochberg.py
@@ -20,7 +20,7 @@ from pkgsim.plot_results import plot_results_all
 
 def main(prt=sys.stdout):
     """Simulate False discovery rate multiple test correction with Benjamini and Hochberg."""
-    perc_sig_list = [0, 5, 10, 20, 35, 50, 75, 95]
+    perc_sig_list = [0, 20, 40, 80]
     num_sims = 100
     global_params = {
         'title':"P-values: {PERC_SIG} Expected Significant (<{MAX_SIG})",
@@ -31,7 +31,8 @@ def main(prt=sys.stdout):
         'dir_img' : "doc/images",
         'alpha' : 0.05,
         'method' : 'fdr_bh'}
-    num_pvalues_list = [10, 20, 50, 100, 500, 1000, 10000]
+    #num_pvalues_list = [10, 20, 50, 100, 500, 1000, 10000]
+    num_pvalues_list = [10, 20]
     results_all = _get_data(perc_sig_list, num_sims, num_pvalues_list, global_params)
     report_results_all(results_all, global_params, prt)
     plot_results_all(results_all, global_params)
@@ -60,16 +61,22 @@ def _run_all_simulations(pars):
 def _run_one_simulation(num_pvalues, perc_sig, params):
     """Generate one set of random pvalues and do multipletest correction."""
     alpha = params['alpha']
+    # 1. Get number of P-values which will be random and which will be set
     num_sig = int(round(float(perc_sig)*num_pvalues/100.0))
     num_rand = num_pvalues - num_sig
     #print "NNNNNNNNN", num_pvalues, "{}%".format(perc_sig), num_sig, num_rand, alpha
+    # 2. Get random P-values and explicitly set P-values
     pvals_rand = np.random.uniform(0, 1, size=num_rand)
     max_sig = 0.05/num_pvalues
     pvals_sig = np.random.uniform(0, max_sig, size=num_sig)
-    #print pvals_rand
-    pvals_all = np.array(list(pvals_rand) + list(pvals_sig))
+    # 3. Add rand/set info to P-values
+    pvals_rand = [(p, True) for p in pvals_rand]
+    pvals_sig = [(p, False) for p in pvals_sig]
+    # 4. Combine random P-values and explictly set P-values
+    pvals_all = list(pvals_rand) + list(pvals_sig)
+    pval_vals = np.array([pval for pval, desc in pvals_all])
     # reject, pvals_corrected, alpha_sidak, alpha_bonf
-    reject, pvals_corrected, _, _ = multipletests(pvals_all, alpha, params['method'])
+    reject, pvals_corrected, _, _ = multipletests(pval_vals, alpha, params['method'])
     return {'pvals':pvals_all, 'reject':reject, 'pvals_corr':pvals_corrected}
 
 if __name__:
--- src/pkgsim/report_results.py
+++ src/pkgsim/report_results.py
@@ -9,7 +9,8 @@ from pkgsim.utils import get_perc_sig, _get_perc_sig
 
 def report_results_all(results_all, global_params, prt=sys.stdout):
     """Report simulation results for many sets of p-values."""
-    pfmt = "{perc_sig:4}=%sig {num_pvalues:6,}=#pvals {num_sims:5}=#sims {alpha:5.2f} {method}\n"
+    pfmt = "{perc_sig:4}=%sig {num_pvalues:6,}=#pvals {num_sims:5}=#sims " \
+           "{alpha:5.2f} {method} {EXP_SIG:5}=#expsig {EXP_RND:5}=#exprnd\n"
     objrpt = StatsDescribe("percs", "{:6.2f}")
     prt.write("%sig #pvals #sims alpha method\n")
     prt.write("---- ------ ----- ----- ------\n")
@@ -17,17 +18,33 @@ def report_results_all(results_all, global_params, prt=sys.stdout):
     aph = global_params['alpha']
     method = global_params['method']
     for perc_sig, num_sims, results_set in results_all:
+        print
         for num_pvalues, results in results_set:
-            prt.write(pfmt.format(
+            # results: list of {'pvals':[(pval0, desc0), (pval1, desc1), ..., 'pvals_corr':..., 'reject':...}
+            num_pvals_sig = int(round(float(num_pvalues)*perc_sig/100))
+            num_pvals_rnd = num_pvalues - num_pvals_sig
+            #exp_num_pass = 
+            msg = pfmt.format(
                 perc_sig=perc_sig,
                 num_pvalues=num_pvalues,
                 num_sims=num_sims,
+                EXP_SIG=num_pvals_sig,
+                EXP_RND=num_pvals_rnd,
                 alpha=aph,
-                method=method))
-            sigs = [(get_perc_sig(r['pvals'], aph), get_perc_sig(r['pvals_corr'], aph)) for r in results]
-            sig_cnt_orig, sig_cnt_corr = zip(*sigs)
-            objrpt.prt_data("orig sig", sig_cnt_orig)
-            objrpt.prt_data("corr sig", sig_cnt_corr)
+                method=method)
+            prt.write(msg)
+            for k2v in results:
+                for (pval, isrand), pval_corr, reject in zip(k2v['pvals'], k2v['pvals_corr'], k2v['reject']):
+                    err_type = get_err_type(reject, isrand)
+                    if err_type[0:1] == "T": # Type I or II error
+                    #if desc == "set" and not reject:
+                        prt.write("{ERRTYPE:7} {PVAL:8.6f} {ISRAND}=is_Rand {PCORR:8.6f} {REJECT:5} {MSG}".format(
+                            ERRTYPE=err_type, PVAL=pval, ISRAND=int(isrand), PCORR=pval_corr, REJECT=reject, MSG=msg))
+            #sigs = [(get_perc_sig(r['pvals'], aph), get_perc_sig(r['pvals_corr'], aph)) for r in results]
+            #sigs = [(get_perc_sig(r['pvals'], aph), get_perc_sig(r['pvals_corr'], aph)) for r in results]
+            #sig_cnt_orig, sig_cnt_corr = zip(*sigs)
+            #objrpt.prt_data("orig sig", sig_cnt_orig)
+            #objrpt.prt_data("corr sig", sig_cnt_corr)
 
 def report_results_each(params, results_allsets, prt):
     """Report one line for each set of p-values."""
@@ -43,5 +60,15 @@ def report_results_one(params, data, prt):
         msg.format(N=num_pval_sig, M=num_pval_tot, P=perc_pval_sig, DESC="original"),
         msg.format(N=num_corr_sig, M=num_corr_tot, P=perc_corr_sig, DESC="corrected")]
     prt.write("{MSG}\n".format(MSG=" ".join(prtmsg)))
+
+def get_err_type(reject, isrand):
+    if not isrand and reject:
+        return ""
+    if isrand and not reject:
+        return ""
+    if isrand and reject:
+        return "Type I"
+    if not isrand and not reject:
+        return "Type II"
 
 # Copyright (C) 2017, DV Klopfenstein. All rights reserved.
