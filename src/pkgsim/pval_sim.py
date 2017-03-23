337b9e1b3af8e55380188e98087d4bfdfbe57336
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2017-03-22 22:48:08 -0400

    Added fnc to get # and % of error Type (I/II/Both or Correct) of one simulation

--- src/pkgsim/pval_sim.py
+++ src/pkgsim/pval_sim.py
@@ -19,10 +19,16 @@ class PvalSim(object):
     """Simulate ONE multiple-test correction of P-values."""
 
     ntobj_mult = cx.namedtuple("NtMult", "reject pvals_corr alpha_sidak alpha_bonf")
-    ntobj_info = cx.namedtuple("Nt", "pval pval_corr reject expsig")
+    ntobj_info = cx.namedtuple("NtResults", "pval pval_corr reject expsig")
+    ntobj_pvaltype = cx.namedtuple("NtResCnts", "act_sig "
+        "num_correct, num_Type_I num_Type_II num_Type_I_II "
+        "perc_correct perc_Type_I perc_Type_II perc_Type_I_II")
 
     def __init__(self, num_pvalues, num_sig, multi_params): #, **kws):
         #self.fnc_maxsig = kws.get('fnc_maxsig', lambda pvals: self.alpha/len(pvals))
+        #self.max_sig = 0.05/num_pvalues
+        self.max_sig = 0.05/num_pvalues
+        #self.max_sig = 0.05/2
         self.multi_params = multi_params
         # Data members: P-value
         self.expsig = None # One for each P-value. True if P-value is intended to be significant.
@@ -31,6 +37,23 @@ class PvalSim(object):
         # Data members: Multipletest correction
         self.ntmult = self._init_ntmult()
         self.pvals_corr = self.ntmult.pvals_corr
+
+    def get_perc_err(self):
+        """Calculate % of corrected p-values with errors: Type I or Type II, Type I, or Type II."""
+        nts = self.get_zipped_data()
+        res_cnt = cx.Counter([self.get_type(nt) for nt in nts])
+        res_cnt[3] = res_cnt[1] + res_cnt[2] # COunt of both error types
+        num_pvals = len(self.pvals)
+        return self.ntobj_pvaltype(
+            act_sig=sum(self.pvals_corr < self.multi_params['alpha']),
+            num_correct=res_cnt[0], 
+            num_Type_I=res_cnt[1], 
+            num_Type_II=res_cnt[2], 
+            num_Type_I_II=res_cnt[3], 
+            perc_correct   = 100.0*res_cnt[0]/num_pvals,
+            perc_Type_I    = 100.0*res_cnt[1]/num_pvals,
+            perc_Type_II   = 100.0*res_cnt[2]/num_pvals,
+            perc_Type_I_II = 100.0*res_cnt[3]/num_pvals)
 
     def get_perc_sig(self, attrname="pvals"): # "pvals" or "pvals_corr"
         """Calculate the percentage of p-values which are significant."""
@@ -49,7 +72,7 @@ class PvalSim(object):
         return [self.ntobj_info._make(vs) for vs in items]
 
     @staticmethod
-    def get_err_type(ntdata):
+    def get_type(ntdata):
         """Return description of the result of one simulation."""
         reject = ntdata.reject
         expsig = ntdata.expsig
@@ -67,8 +90,7 @@ class PvalSim(object):
         # 1. Get random P-values and explicitly set P-value
         #   False -> If P-value is significant, it is by chance
         #   True  -> P-value is intended to be significant
-        max_sig = 0.05/num_pvalues
-        pvals_sig = [(p, True) for p in np.random.uniform(0, max_sig, size=num_sig)]
+        pvals_sig = [(p, True) for p in np.random.uniform(0, self.max_sig, size=num_sig)]
         pvals_rnd = [(p, False) for p in np.random.uniform(0, 1, size=num_rand)]
         # 2. Combine random P-values and explictly set P-value
         pvals_expsig = list(pvals_sig) + list(pvals_rnd)
