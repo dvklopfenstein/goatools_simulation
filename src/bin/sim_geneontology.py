b86baff3054e6158db9f75e7564f4daa9efa7392
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2017-05-25 22:55:02 -0400

    Now running a number of sims. Next: more informative report.

--- src/bin/sim_geneontology.py
+++ src/bin/sim_geneontology.py
@@ -45,10 +45,12 @@ __author__ = "DV Klopfenstein"
 
 import sys
 import datetime
+from random import shuffle
 
 #from pkgsim.data_geneids_rich_immune import geneids as genes_immune
 from pkgsim.genes_rich_immune import genes as GENES
 from pkgsim.utils import shuffle_associations
+from pkgsim.randseed import RandomSeed32
 
 from goatools_suppl.data.ensmusg2sym import ensm2sym
 from goatools_suppl.proj_data import GoatoolsDataMaker
@@ -79,21 +81,21 @@ class GoeaSimObj(object):
         return "alpha({A}) method({M})".format(A=self.alpha, M=self.method)
 
 
-def main(prt=sys.stdout):
+def main(seed, prt=sys.stdout):
     """Return a list of all GO IDs associated with protein-coding mouse genes."""
+    seed = RandomSeed32(seed)
     # 1. Get objects needed for a gene-ontology simulation: pop_genes, assc, GO-DAG
     obj = GoeaSimObj(alpha=0.05, method='fdr_bh')
-    genes_mus = set(ensm2sym.keys()) # Population genes
-    genes_immune = GENES             # Study genes
+    genes_mus = ensm2sym.keys()  # Population genes
+    genes_immune = list(GENES)   # Study genes
     assoc_ens2gos = GoatoolsDataMaker.get_assoc("gene_association.mgi", genes_mus)
     # 2. SIMULATE SIGNIFICANCE TO IMMUNE: Population is all mouse genes
-    results_sig = []
-    results_sig.append(run_actual_assc(obj, assoc_ens2gos, genes_mus, genes_immune))
+    results_sig = run_assc(run_actual_assc, obj, assoc_ens2gos, genes_mus, genes_immune)
     # 3. SIMULATE NO SIGNIFICANCE:
-    results_rnd = []
-    results_rnd.append(run_random_assc(obj, assoc_ens2gos, genes_mus, genes_immune))
+    results_rnd = run_assc(run_random_assc, obj, assoc_ens2gos, genes_mus, genes_immune)
     # 4. REPORT RESULTS:
     rpt_results(prt, obj, results_sig, results_rnd)
+    seed.prt(prt)
 
 def rpt_results(prt, obj, results_sig, results_rnd):
     """Report results GOEAs on actual associations and randon associations."""
@@ -108,30 +110,44 @@ def rpt_results(prt, obj, results_sig, results_rnd):
     for rnd in results_rnd:
         prt.write(txt.format(N=len(rnd['goea_results']), M=len(rnd['genes']), DESC="random"))
 
+def run_assc(run_fnc, obj, assoc_ens2gos, genes_mus, genes_immune):
+    """Run GOEAs on incrementing study sizes with randomly selected study genes."""
+    results = []
+    for exp in reversed(range(2, 12)):  # 4, 8, ..., 256, 512, 1024, 2048
+        shuffle(genes_immune)
+        qty = pow(2, exp)
+        results.append(run_fnc(obj, assoc_ens2gos, genes_mus, genes_immune[:qty]))
+    return results
+
 def run_actual_assc(obj, assoc_ens2gos, genes_mus, genes_immune):
     """SIMULATE SIGNIFICANCE TO IMMUNE."""
+    genes_immune = set(genes_immune)
     goeaobj = obj.get_goeaobj(genes_mus, assoc_ens2gos)
     goea_results = goeaobj.run_study(genes_immune, keep_if=lambda nt: nt.p_fdr_bh < 0.05)
-    goeaobj.wr_txt("goea_immune.txt", goea_results)
-    genes = get_study_items(goea_results)
-    assert genes_immune == genes, \
-        "EXPECTED ALL {S} STUDY GENES TO SHOW SIGNIFICANT GO TERMS. FOUND {M}".format(
-            S=len(genes_immune), M=len(genes_immune.difference(genes)))
-    return {'goea_results':goea_results, 'genes':genes}
+    fout_txt = "goea_immune_{N:04}.txt".format(N=len(genes_immune))
+    goeaobj.wr_txt(fout_txt, goea_results)
+    genes_sig = get_study_items(goea_results)
+    if genes_immune != genes_sig:
+        msg = "EXPECTED ALL {S} STUDY GENES TO SHOW SIGNIFICANT GO TERMS. FOUND {M}\n"
+        sys.stdout.write(msg.format(
+            S=len(genes_immune), M=len(genes_immune.difference(genes_sig))))
+    return {'goea_results':goea_results, 'genes':genes_sig}
 
 def run_random_assc(obj, assoc_ens2gos, genes_mus, genes_immune):
     """SIMULATE NO SIGNIFICANCE."""
     rand_assoc = shuffle_associations(assoc_ens2gos)
     goeaobj = obj.get_goeaobj(genes_mus, rand_assoc)
     goea_results = goeaobj.run_study(genes_immune, keep_if=lambda nt: nt.p_fdr_bh < 0.05)
-    goeaobj.wr_txt("goea_immune.txt", goea_results)
-    genes = get_study_items(goea_results)
+    fout_txt = "goea_immune_{N:04}.txt".format(N=len(genes_immune))
+    goeaobj.wr_txt(fout_txt, goea_results)
+    genes_rnd = get_study_items(goea_results)
     assert len(goea_results) == 0, \
         "EXPECTED NO SIGNIFICANT GO TERMS IN RANDOM SIMULATION. FOUND {N}".format(
             N=len(goea_results))
-    return {'goea_results':goea_results, 'genes':genes}
+    return {'goea_results':goea_results, 'genes':genes_rnd}
 
 if __name__ == '__main__':
-    main()
+    SEED = int(sys.argv[1], 0) if len(sys.argv) != 1 else None
+    main(SEED)
 
 # Copyright (C) 2016-2017, DV Klopfenstein. All rights reserved.
