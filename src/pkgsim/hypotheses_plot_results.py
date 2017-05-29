21d709ed98bcc17837080af98ccfd34224e2f9c1
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2017-05-29 03:38:02 -0400

    make main plot xy-labels bigger. pylint.

--- src/pkgsim/hypotheses_plot_results.py
+++ src/pkgsim/hypotheses_plot_results.py
@@ -8,7 +8,7 @@ import sys
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
+#import numpy as np
 
 def plot_results_all(objsim, params):
     """Plot simulation results for many sets of p-values."""
@@ -97,7 +97,7 @@ def wrpng_boxplot_sigs_each(dfrm, alpha, **kws):
         plt.show()
 
 def set_axis_boxplot(ax_boxplot, dfrm, alpha, **kws):
-    """Return axis for one boxplot of simulated FDRs."""
+    """Fills axis ax_boxplot with one set of boxplots of simulated FDRs."""
     dotsz = kws.get('dotsize', 5)
     sns.stripplot(x="xval", y="yval", data=dfrm, jitter=True, size=dotsz, ax=ax_boxplot)
     sns.boxplot(x="xval", y="yval", hue="group", data=dfrm, palette="PRGn", ax=ax_boxplot)
@@ -107,16 +107,13 @@ def set_axis_boxplot(ax_boxplot, dfrm, alpha, **kws):
         line.set_color('red' if is_median else 'black')
         if is_median:
             line.set_linestyle('--')
-    ax_boxplot.plot([-1000, 1000], [alpha, alpha], 'b.-')
+    ax_boxplot.plot([-1000, 1000], [alpha, alpha], 'b--')
     if 'ylim_a' in kws and 'ylim_b' in kws:
         ax_boxplot.set_ylim(kws['ylim_a'], kws['ylim_b'])
     if 'title' in kws:
         ax_boxplot.set_title(kws['title'], size=25)
-    if 'xlabel' in kws:
-        ax_boxplot.set_xlabel(kws['xlabel'], size=20)
-    if 'ylabel' in kws:
-        #ax_boxplot.set_ylabel("Simulated FDR Ratios", size=20)
-        ax_boxplot.set_ylabel(kws['ylabel'], size=20)
+    ax_boxplot.set_xlabel(kws.get('xlabel', ""), size=20)
+    ax_boxplot.set_ylabel(kws.get('ylabel', ""), size=20)
     return ax_boxplot
 
 def get_percsig_dicts(numpvals_sims):
@@ -151,26 +148,6 @@ def plt_box_all(fimg_pat, key2exps, attrname='fdr_actual', grpname='FDR'):
         dfrm = pd.DataFrame(_get_dftbl_boxplot(expsets, attrname, grpname))
         wrpng_boxplot_sigs_each(dfrm, expsets[0].alpha, **kws)
 
-# TBD: rm
-def plt_box_tiled1(fout_img, key2exps, attrname='fdr_actual', grpname='FDR'):
-    """Plot all detailed boxplots for all experiments. X->(maxsigval, #pvals), Y->%sig"""
-    kws = {
-        'fout_img': None,
-        'xlabel': 'Number of Tested Hypotheses',
-        'ylabel': 'Simulated FDR Ratios',
-        'ylim_a':0, 'ylim_b':0.10}
-    perc_sigs, max_sigpvals = _get_num_rows_cols(key2exps)
-    num_rows = len(perc_sigs)
-    num_cols = len(max_sigpvals)
-    plt.close('all')
-    fig, axes_2d = plt.subplots(num_rows, num_cols, sharex='col', sharey='row')
-    for row, perc_sig in enumerate(perc_sigs):
-        for col, max_sigpval in enumerate(max_sigpvals):
-            expsets = key2exps[(perc_sig, max_sigpval)]
-            dfrm = pd.DataFrame(_get_dftbl_boxplot(expsets, attrname, grpname))
-            set_axis_boxplot(axes_2d[row][col], dfrm, expsets[0].alpha, **kws)
-    plt.show()
-
 def plt_box_tiled(fout_img, key2exps, attrname='fdr_actual', grpname='FDR'):
     """Plot all detailed boxplots for all experiments. X->(maxsigval, #pvals), Y->%sig"""
     kws = {
@@ -180,16 +157,13 @@ def plt_box_tiled(fout_img, key2exps, attrname='fdr_actual', grpname='FDR'):
         'ylim_a':0, 'ylim_b':0.09}
     perc_sigs, max_sigpvals = _get_num_rows_cols(key2exps)
     num_plts = len(key2exps)
-    rows = len(perc_sigs)
-    cols = len(max_sigpvals)
+    num_rows = len(perc_sigs)
+    num_cols = len(max_sigpvals)
     plt.close('all')
     # https://stackoverflow.com/questions/6963035/pyplot-axes-labels-for-subplots
     fig = plt.figure()
     axall = fig.add_subplot(1, 1, 1)
-    ax1 = fig.add_subplot(rows, cols, 1)
-    #fig, axes_2d = plt.add_subplots(num_rows, num_cols, sharex='col', sharey='row')
-    axes_2d = [ax1] + [fig.add_subplot(rows, cols, i, sharex=ax1, sharey=ax1) for i in range(2, num_plts+1)]
-    #axes_2d = fig.add_subplot(rows, cols, 1, sharex='col', sharey='row')
+    axes_2d = _get_tiled_axes(fig, num_rows, num_cols)
     idx = 0
     for row, perc_sig in enumerate(perc_sigs):
         for col, max_sigpval in enumerate(max_sigpvals):
@@ -198,44 +172,80 @@ def plt_box_tiled(fout_img, key2exps, attrname='fdr_actual', grpname='FDR'):
             #set_axis_boxplot(axes_2d[row][col], dfrm, expsets[0].alpha, **kws)
             set_axis_boxplot(axes_2d[idx], dfrm, expsets[0].alpha, **kws)
             idx += 1
+    # Turn off xticklabels and yticklabels on the inside plot edges
+    _tiled_xylabels_off(axes_2d, num_cols)
     # https://stackoverflow.com/questions/3584805/in-matplotlib-what-does-the-argument-mean-in-fig-add-subplot111
     # https://matplotlib.org/examples/pylab_examples/shared_axis_demo.html
     # https://stackoverflow.com/questions/1358977/how-to-make-several-plots-on-a-single-page-using-matplotlib
     # https://stackoverflow.com/questions/6963035/pyplot-axes-labels-for-subplots
-    axall.set_xlabel('Number of Tested Hypotheses')
-    axall.set_ylabel('Simulated {GRP} Ratios'.format(GRP=grpname))
+    axall.set_xlabel('Number of Tested Hypotheses', size=30)
+    axall.set_ylabel('Simulated {GRP} Ratios'.format(GRP=grpname), size=30)
     plt.show()
+
+def _get_tiled_axes(fig, n_rows, n_cols):
+    """Create empty axes to be filled and used in tiled boxplot image."""
+    ax1 = fig.add_subplot(n_rows, n_cols, 1)
+    rng = range(2, n_rows*n_cols+1)
+    return [ax1] + [fig.add_subplot(n_rows, n_cols, i, sharex=ax1, sharey=ax1) for i in rng]
+
+def _tiled_xylabels_off(axes, num_cols):
+    """Turn off xticklabels and yticklabels on the inside plot edges of the tiled boxplots."""
+    for xaxis in axes[:-1*num_cols]:
+        xaxis.set_xticklabels([])
+    for idx, yaxis in enumerate(axes):
+        if idx%num_cols != 0:
+            yaxis.set_yticklabels([])
 
 def _get_num_rows_cols(key2exps):
     """Given the list of experiment sets, return the number of rows and columns for plotting."""
     perc_sigs, max_sigpval = zip(*key2exps.keys())
     return sorted(set(perc_sigs)), sorted(set(max_sigpval))
 
-# TBD: rm
-def plt_box_tiled_plain(fout_img, key2exps, attrname='fdr_actual', grpname='FDR'):
-    """Plot all plain boxplots for all experiments. X->(maxsigval, #pvals), Y->%sig"""
-    dpi = 200
-    dfrm = pd.DataFrame(_get_dftbl_boxplots(key2exps, attrname, grpname))
-    grd = sns.FacetGrid(dfrm, col='max_sigpval', row='perc_true_null')
-    grd = grd.map(sns.boxplot, "xval", "yval")
-    plt.title('{GRP} Simulations'.format(GRP=grpname))
-    plt.xlabel('Number of Tested Hypotheses')
-    plt.ylabel('Simulated {GRP}s'.format(GRP=grpname))
-    #plt.show()
-    plt.savefig(fout_img, dpi=dpi)
-    sys.stdout.write("  WROTE: {IMG}\n".format(IMG=fout_img))
+#### # TBD: rm
+#### def plt_box_tiled1(fout_img, key2exps, attrname='fdr_actual', grpname='FDR'):
+####     """Plot all detailed boxplots for all experiments. X->(maxsigval, #pvals), Y->%sig"""
+####     kws = {
+####         'fout_img': None,
+####         'xlabel': 'Number of Tested Hypotheses',
+####         'ylabel': 'Simulated FDR Ratios',
+####         'ylim_a':0, 'ylim_b':0.10}
+####     perc_sigs, max_sigpvals = _get_num_rows_cols(key2exps)
+####     num_rows = len(perc_sigs)
+####     num_cols = len(max_sigpvals)
+####     plt.close('all')
+####     fig, axes_2d = plt.subplots(num_rows, num_cols, sharex='col', sharey='row')
+####     for row, perc_sig in enumerate(perc_sigs):
+####         for col, max_sigpval in enumerate(max_sigpvals):
+####             expsets = key2exps[(perc_sig, max_sigpval)]
+####             dfrm = pd.DataFrame(_get_dftbl_boxplot(expsets, attrname, grpname))
+####             set_axis_boxplot(axes_2d[row][col], dfrm, expsets[0].alpha, **kws)
+####     plt.show()
 
-# TBD: rm
-def _get_dftbl_boxplots(key2exps, attrname='fdr_actual', grpname='FDR'):
-    """Get data for dataframe for multiple boxplot tiles."""
-    tbl_full = []
-    for (perc_sig, max_sigpval), expsets in key2exps.items():
-        perc_true_null = 100 - perc_sig
-        for dct in _get_dftbl_boxplot(expsets, attrname, grpname):
-            dct['max_sigpval'] = max_sigpval
-            dct['perc_true_null'] = perc_true_null
-            tbl_full.append(dct)
-    return tbl_full
+#### # TBD: rm
+#### def plt_box_tiled_plain(fout_img, key2exps, attrname='fdr_actual', grpname='FDR'):
+####     """Plot all plain boxplots for all experiments. X->(maxsigval, #pvals), Y->%sig"""
+####     dpi = 200
+####     dfrm = pd.DataFrame(_get_dftbl_boxplots(key2exps, attrname, grpname))
+####     grd = sns.FacetGrid(dfrm, col='max_sigpval', row='perc_true_null')
+####     grd = grd.map(sns.boxplot, "xval", "yval")
+####     plt.title('{GRP} Simulations'.format(GRP=grpname))
+####     plt.xlabel('Number of Tested Hypotheses')
+####     plt.ylabel('Simulated {GRP}s'.format(GRP=grpname))
+####     #plt.show()
+####     plt.savefig(fout_img, dpi=dpi)
+####     sys.stdout.write("  WROTE: {IMG}\n".format(IMG=fout_img))
+
+#### # TBD: rm
+#### def _get_dftbl_boxplots(key2exps, attrname='fdr_actual', grpname='FDR'):
+####     """Get data for dataframe for multiple boxplot tiles."""
+####     tbl_full = []
+####     for (perc_sig, max_sigpval), expsets in key2exps.items():
+####         perc_true_null = 100 - perc_sig
+####         for dct in _get_dftbl_boxplot(expsets, attrname, grpname):
+####             dct['max_sigpval'] = max_sigpval
+####             dct['perc_true_null'] = perc_true_null
+####             tbl_full.append(dct)
+####     return tbl_full
 
 def _get_dftbl_boxplot(experimentsets, attr='fdr_actual', grp='FDR'):
     """Get plotting data suitable for a single plot of boxplots."""
