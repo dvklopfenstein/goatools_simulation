970d205e2d3296afc3feb60d10db4f2b707f956e
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2017-06-02 08:53:42 -0400

    Now randomly creating sets of studygenes w/user-specfied "True Null" qty.

--- src/pkggosim/goea_sim.py
+++ src/pkggosim/goea_sim.py
@@ -5,6 +5,9 @@ __author__ = "DV Klopfenstein"
 
 import sys
 import collections as cx
+from random import shuffle
+from goatools.go_enrichment import get_study_items
+from pkggosim.utils import get_result_desc
 
 class GoeaSim(object):
     """Simulate a Gene Ontology Enrichment Analysis (GOEA) on one set of randomly generated study genes."""
@@ -17,9 +20,9 @@ class GoeaSim(object):
         #self.alpha = objbg['alpha']
         iniobj = _Init(num_study_genes, num_null, study_genes_bg, objbg)
 #        # List of info for each pval: pval pval_corr reject expsig tfpn
-#        self.nts_pvalmt = iniobj.get_nts_pvals()
-#        self.pvals = np.array(iniobj.pvals)
-#        self.pvals_corr = np.array(iniobj.ntmult.pvals_corr)
+#        self.nts_pvalmt = iniobj.get_nts_stugenes()
+#        self.genes_stu = np.array(iniobj.pvals)
+#        self.genes_stu_corr = np.array(iniobj.ntmult.pvals_corr)
 #        # One namedtuple summarizing results of this P-Value simulation
 #        self.nt_tfpn = self.get_nt_tfpn()
 #
@@ -113,74 +116,57 @@ class GoeaSim(object):
 class _Init(object):
     """Init GoeaSim object: Create random hypotheses test results(pvals), run multipletest correction."""
 
-#    # Multiple test correction results:
-#    #   1. statsmodels multiple test results for each P-value
-#    _ntobj_mtsm = cx.namedtuple("NtMtStatmod", "reject pvals_corr alpha_sidak alpha_bonf")
-#    #   2. summarized results for each P-value
-#    ntobj_mt = cx.namedtuple("NtMtPvals", "pval pval_corr reject expsig tfpn")
-#
-#    @staticmethod
-#    def get_result_desc(reject, expsig):
-#        """Return description of the result of one simulation."""
-#        # pylint: disable=multiple-statements
-#        #
-#        # TABLE 1) Number of errors committed when testing m null hypotheses
-#        # 1995; Yoav Benjamini and Yosef Hochberg:
-#        #
-#        #                 reject ->| False         | True
-#        #
-#        #                          |Declared       | Declared      |
-#        #                          |non-significant| significant   | Total
-#        # -------------------------+---------------+---------------+--------
-#        # True null hypotheses     | U TN          | V FP (Type I) |   m(0)
-#        # Non-true null hypotheses | T FN (Type II)| S TP          | m - m(0)
-#        #                          |      m - R    |       R       |   m
-#        if     expsig and     reject: return "TP" # Correct:     Significant
-#        if not expsig and not reject: return "TN" # Correct: Not Significant
-#        if not expsig and     reject: return "FP" # Type  I Error (False Positive)
-#        if     expsig and not reject: return "FN" # Type II Error (False Negative)
-#        assert True, "UNEXPECTED ERROR TYPE"
-#        # Random variable, Q = V/(V+S); Q = 0 when V+S = 0, is the proportion of errors committed by
-#        # falsely rejecting null hypotheses.
-#        # Power = 1 - beta; Beta < 20% good
-#        # average power: the proportion of the false hypotheses which are correctly rejected
-#        # TP/(FN + TP)
-#
-#    def get_nts_pvals(self):
-#        """Combine data to return nts w/fields: pvals, pvals_corr, reject, expsig."""
-#        pvalsim_results = []
-#        items = zip(self.pvals, self.ntmult.pvals_corr, self.ntmult.reject, self.expsig)
-#        for pval_orig, pval_corr, reject, expsig in items:
-#            pvalsim_results.append(self.ntobj_mt(
-#                pval      = pval_orig,
-#                pval_corr = pval_corr,
-#                reject    = reject,
-#                expsig    = expsig, # False->True Null; True->Non-true null
-#                tfpn      = self.get_result_desc(reject, expsig))) # Ex: TP, TN, FP, or FN
-#        return pvalsim_results
+    #ntobj_mt = cx.namedtuple("NtMtPvals", "pval pval_corr reject expsig tfpn")
+    ntobj_mt = cx.namedtuple("NtMtPvals", "study_gene     reject expsig tfpn")
+
+    def get_nts_stugenes(self):
+        """Combine data to return nts w/fields: pvals, pvals_corr, reject, expsig."""
+        goeasim_results = []
+        for study_gene, expsig in zip(self.genes_stu, self.expsig):
+            reject = study_gene in self.genes_sig
+            goeasim_results.append(self.ntobj_mt(
+                study_gene = study_gene,
+                reject     = reject,
+                expsig     = expsig, # False->True Null; True->Non-true null
+                tfpn       = get_result_desc(reject, expsig))) # Ex: TP, TN, FP, or FN
+        return goeasim_results
 
     def __init__(self, num_study_genes, num_null, study_genes_bg, objbg):
         self.objbg = objbg
-        # I. UNCORRECTED P-VALUES:
-        self.pvals = None  # List of randomly-generated uncorrected P-values
-        self.expsig = None # List of bool/P-value. True -> P-value is intended to be significant (Non-true null)
-#        self._init_pvals(num_study_genes, num_null)
-#        assert len(self.pvals) == num_study_genes
-#        assert num_study_genes - sum(self.expsig) == num_null
-#        # II. P-VALUES CORRECTED BY MULTIPLE-TEST CORRECTION:
-#        # Run a multipletest correction on this set of pvals
-#        self.ntmult = self._ntobj_mtsm._make(multipletests(self.pvals, **self.objbg))
+        # I. Genes in two groups: Different than population AND no different than population
+        self.genes_stu = None  # List of randomly-generated gene lists
+        self.expsig = None # List of bool/gene. True -> gene is intended to be significant (Non-true null)
+        self._init_study_genes(num_study_genes, num_null, study_genes_bg)
+        assert len(self.genes_stu) == num_study_genes
+        assert num_study_genes - sum(self.expsig) == num_null
+        goea_results = self._init_goea_results()
+        self.genes_sig = get_study_items(goea_results)
+        print "STUDY({}) SIG({}) NULL({})".format(len(self.genes_stu), len(self.genes_sig), num_null)
+#        self.ntmult = self._ntobj_mtsm._make(multipletests(self.genes_stu, **self.objbg))
 
-#    def _init_pvals(self, num_study_genes, num_null):
-#        """Generate 2 sets of P-values: Not intended significant & intended to be significant."""
-#        num_ntnull = num_study_genes - num_null # Calculate the number of "Non-true null hypotheses"
-#        # 1. Generate random P-values: Significant and Random
-#        #   True  -> P-value is intended to be significant
-#        #   False -> If P-value is significant, it occured by chance
-#        pvals_expsig = \
-#            [(p, True) for p in np.random.uniform(0, self.max_sigval, size=num_ntnull)] + \
-#            [(p, False) for p in np.random.uniform(0, 1, size=num_null)]
-#        # 2. Extract "P-values" and "intended significance" by transposing data
-#        self.pvals, self.expsig = zip(*pvals_expsig)
+    def _init_goea_results(self):
+        """Run Gene Ontology Analysis."""
+        attrname = "p_{METHOD}".format(METHOD=self.objbg.objbg.method)
+        alpha = self.objbg.objbg.alpha
+        keep_if = lambda nt: getattr(nt, attrname) < alpha
+        return self.objbg.objgoea.run_study(self.genes_stu, keep_if=keep_if)
+
+    def _init_study_genes(self, num_study_genes, num_null, genes_study_bg):
+        """Generate 2 sets of genes: Not intended significant & intended to be significant."""
+        # Calculate the number of "Non-true null hypotheses":
+        #   Study genes found to be different than the population genes likely not by chance
+        num_ntnull = num_study_genes - num_null
+        # 1. Generate random genes: Significant and Random
+        #   True  -> gene is intended to be significant(different from the population)
+        #   False -> If gene is determined significant, it occured by chance
+        genes_pop_bg = list(self.objbg.pop_genes.difference(genes_study_bg))
+        genes_study_bg = list(genes_study_bg)
+        shuffle(genes_pop_bg)
+        shuffle(genes_study_bg)
+        genes_expsig = \
+            [(g, True) for g in genes_study_bg[:num_ntnull]] + \
+            [(g, False) for g in genes_pop_bg[:num_null]]
+        # 2. Extract "genes" and "intended significance" by transposing data
+        self.genes_stu, self.expsig = zip(*genes_expsig)
 
 # Copyright (C) 2016-2017, DV Klopfenstein, Haibao Tang. All rights reserved.
