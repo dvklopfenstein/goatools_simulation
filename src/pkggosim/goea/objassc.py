ea260198c7a5e1962b20fb2cebe65fd330376726
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2017-06-08 18:04:51 -0400

    Save go2genes instead of set of gos.

--- src/pkggosim/goea/objassc.py
+++ src/pkggosim/goea/objassc.py
@@ -4,7 +4,6 @@ __cright__ = "Copyright (C) 2016-2017, DV Klopfenstein, Haibao Tang. All rights 
 __author__ = "DV Klopfenstein"
 
 import collections as cx
-from pkggosim.goea.objbase import DataBase
 from pkggosim.goea.utils import get_assoc_data, get_assoc_hdr
 
 class DataAssc(object):
@@ -12,9 +11,7 @@ class DataAssc(object):
 
     ntdesc = cx.namedtuple("results", "name perc_null tot_study")
 
-    #### def __init__(self, alpha, method, pop_genes, assc_file):
     def __init__(self, assc_file, pop_genes):
-        #### self.objbase = DataBase(alpha, method)
         # Read the association file. Save GOs related to population genes
         assc_geneid2gos = get_assoc_data(assc_file, pop_genes)
         # Simplify sim analysis: Use population genes found in association for GOEA Sim eval
@@ -22,22 +19,23 @@ class DataAssc(object):
         # Speed sims: Use the association subset actually in the population
         self.assc_hdr = get_assoc_hdr(assc_file)
         self.assc = {g:gos for g, gos in assc_geneid2gos.items() if g in self.pop_genes}
-        #### self.objgoea = self.objbase.get_goeaobj(self.pop_genes, self.assc) # GOEnrichmentStudy obj
+        self.go2genes = self.get_go2genes()
 
     def prt_summary(self, prt):
         """Print summary of parameters and background data."""
-        #### self.objbase.prt_summary(prt)
         prt.write("\nASSOCIATION INFORMATION:\n    ")
         prt.write("{ASSC}\n\n".format(ASSC="\n    ".join(self.assc_hdr.split("\n"))))
         prt.write("{N:6,} GENES  IN ASSOCIATION\n".format(N=len(self.assc)))
-        prt.write("{N:6,} GO IDs IN ASSOCIATION\n".format(N=len(self.get_assc_goids())))
+        prt.write("{N:6,} GO IDs IN ASSOCIATION\n".format(N=len(self.go2genes.keys())))
         prt.write("{N:6,} GENES  IN POPULATION\n".format(N=len(self.pop_genes)))
 
-    def get_assc_goids(self):
-        """Return the GO IDs used in the simulation."""
-        goids_all = set()
-        for goids_gene in self.assc.values():
-            goids_all |= goids_gene
-        return goids_all
+    def get_go2genes(self, geneids=None):
+        """Return association (gene2gos) as go2genes."""
+        go2genes = cx.defaultdict(set)
+        for gene, goids in self.assc.items():
+            if geneids is None or gene in geneids:
+                for goid in goids:
+                    go2genes[goid].add(gene)
+        return go2genes
 
 # Copyright (C) 2016-2017, DV Klopfenstein, Haibao Tang. All rights reserved.
