5180cf5c3ea7814c703370794594dd3e794eb052
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2017-06-10 13:46:07 -0400

    Going to ditch this test

--- src/tests/test_goea_sims.py
+++ src/tests/test_goea_sims.py
@@ -6,39 +6,34 @@ __author__ = "DV Klopfenstein"
 
 import sys
 from pkggosim.goea.sims import ManyGoeaSims
-from pkggosim.goea.utils import get_study2genes, get_assoc_data
+from pkggosim.goea.utils import get_assoc_data
 from pkggosim.goea.objassc import DataAssc
 from goatools_suppl.data.ensmusg2sym import ensm2sym
+from pkggosim.goea.utils import get_genes, get_genes_all
 
 def main(prt=sys.stdout):
     """Test ManyGoeaSims in ./src/pkggosim.goea.sims.py"""
-    alpha = 0.05
-    method = 'fdr_bh'
-
-
     study_genes_lens = [4, 8, 16, 32, 64]
     num_study_genes = study_genes_lens[2]
     num_null = num_study_genes/2 # 50% Null
     # Use study genes seen in population
-    study_genes_bg = [g for g in study2genes['humoral_rsp'] if g in objbg.pop_genes]
+    study_genes_bg = get_genes('humoral_rsp')
+    genes_mus = ensm2sym.keys()  # Population genes
+    assc_geneid2gos = get_assoc_data("gene_association.mgi", genes_mus)
+    objbg = DataAssc(0.05, 'fdr_bh', genes_mus, assc_geneid2gos)
 
     params = {
+        'randomize_truenull_assc':False,
         'num_sims':10,
         'num_study_genes':16,
         'perc_null':50,
         'num_null':8,
-        'study_genes_bg':study_genes_bg}
+        'study_genes_bg':study_genes_bg,
+        'genes_population':}
     objsim = ManyGoeaSims(params, study_genes_bg, objbg)
     objsim.prt_summary(prt)
     for nt_tfpn in objsim.nts_tfpn:
         print nt_tfpn
-
-def get_objbg():
-    """Return object holding all params/data structures used in all sims (eg GO-DAG)."""
-    study2genes = get_study2genes()
-    genes_mus = ensm2sym.keys()  # Population genes
-    assc_geneid2gos = get_assoc_data("gene_association.mgi", genes_mus)
-    objbg = DataAssc(alpha, method, genes_mus, assc_geneid2gos)
 
 if __name__ == '__main__':
     main()
