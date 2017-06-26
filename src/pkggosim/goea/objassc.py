860b42225894c5a82073b640551b670476d566a9
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2017-06-26 09:43:18 -0400

    Add split_assc for rnd_tgtd. Correct randomization.

--- src/pkggosim/goea/objassc.py
+++ src/pkggosim/goea/objassc.py
@@ -40,12 +40,18 @@ class DataAssc(object):
                     go2genes[goid].add(gene)
         return go2genes
 
-    def rm_goids(self, genes_nontruenull, goids_torm):
-        """Remove specified GO IDs from all gene associations."""
+    def split_assc(self, goids_tgtd_all):
+        """Remove targeted GO IDs from all gene associations. Place in new sub-assc."""
         assc_pruned = {}
-        for geneid, goids in self.assc.items():
-            assc_pruned[geneid] = goids.difference(goids_torm)
-        return assc_pruned 
+        assc_tgtd = {}
+        for geneid, goids_gene in self.assc.items():
+            goids_tgtd_cur = goids_gene.intersection(goids_tgtd_all)
+            if goids_tgtd_cur:
+                assc_pruned[geneid] = goids_gene.difference(goids_tgtd_cur)
+                assc_tgtd[geneid] = goids_tgtd_cur
+            else:
+                assc_pruned[geneid] = goids_gene
+        return assc_pruned, assc_tgtd
 
     def get_randomized_assc(self, genes_truenull, genes_nontruenull):
         """Randomize assc. for only all "True Null" genes."""
@@ -61,25 +67,72 @@ class DataAssc(object):
             assc_rand[gene] = self.assc[gene]
         return assc_rand
 
-    @staticmethod
-    def shuffle_associations(assoc_ens2gos):
+    def shuffle_associations(self, assc_geneid2gos):
         """Randomly shuffle all associations to mimic a list of genes having no significance."""
         # Get a list of GO-list length of all genes
-        goset_lens = [len(gos) for gos in assoc_ens2gos.values()]
-        gos_all_set = set()
-        for gos_gene in assoc_ens2gos.values():
-            gos_all_set |= gos_gene
-        gos_all_lst = list(gos_all_set)
-        # Randomly shuffle GOs
-        shuffle(goset_lens)
-        shuffle(gos_all_lst)
+        gos_2d = assc_geneid2gos.values()
         # Build new randomly shuffled association
-        assc_rand = {}
-        idx_start = 0
-        for geneid, golen in zip(assoc_ens2gos.keys(), goset_lens):
-            idx_stop = idx_start + golen
-            assc_rand[geneid] = gos_all_lst[idx_start:idx_stop]
-            idx_start = idx_stop
+        genes = assc_geneid2gos.keys()
+        golens = [len(gos) for gos in gos_2d]
+        shuffle(golens)
+        # Get Random assc #1
+        goctr_rand = self._mk_rand_gocnts(gos_2d)
+        assc_rand = {g:self._get_rand_goids(n, goctr_rand) for g, n in zip(genes, golens)}
+        # assc_rand = {g:assc_geneid2gos[g] for g, n in zip(genes, golens)}
+        # assc_rand = self._simple_assc_rand(genes, golens, gos_2d)
         return assc_rand
+
+####    def _simple_assc_rand(genes, golens, gos_2d):
+####        """Long sim times."""
+####        goids = self._get_goids_assc(gos_2d)
+####        assc_rand = {}
+####        for geneid, num in zip(genes, golens):
+####            shuffle(goids)
+####            assc_rand[geneid] = goids[:num]
+####        return assc_rand
+
+####    def _get_goids_assc(self, gos_2d):
+####        """Get list of all GO IDs."""
+####        goids = set()
+####        for gos in gos_2d:
+####            goids |= gos
+####        return list(goids)
+
+    @staticmethod
+    def _mk_rand_gocnts(gos_2d):
+        """Use GO IDs in current asscs to create randomized list of GO IDs for new rand assc."""
+        # Get current counts of GO IDs in an association
+        ctr_orig = cx.Counter()
+        for gos_gene in gos_2d:
+            for goid in gos_gene:
+                ctr_orig[goid] += 1
+        # Create new randomized counts of GO IDs in an association
+        goids, cnts = zip(*ctr_orig.items())
+        goids = list(goids)
+        cnts = list(cnts)
+        shuffle(goids)
+        shuffle(cnts)
+        ctr_rand = cx.Counter({g:c for g, c in zip(goids, cnts)})
+        assert sum(ctr_orig.values()) == sum(ctr_rand.values())
+        assert len(ctr_orig) == len(ctr_rand)
+        return ctr_rand
+
+    @staticmethod
+    def _get_rand_goids(num_goids, goctr):
+        """Return N randomly selected GO IDs. Adjust counts."""
+        goids_all = list(goctr)
+        assert goids_all
+        shuffle(goids_all)
+        # Choose a subset
+        goids_cur = set(goids_all[:num_goids])
+        if num_goids > len(goids_all):
+            assert len(goids_all) < 1000, "RQ({}) OF {}".format(num_goids, len(goids_all))
+        # Adjust counter values
+        for goid in goids_cur:
+            if goctr[goid] != 1:
+                goctr[goid] -= 1
+            else:
+                del goctr[goid]
+        return goids_cur
 
 # Copyright (C) 2016-2017, DV Klopfenstein, Haibao Tang. All rights reserved.
